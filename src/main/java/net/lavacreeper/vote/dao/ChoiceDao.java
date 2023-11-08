6c787f94cf17ebc0793556aeed86af7f10bc6ca8
lavacreeper <lavacreeper2333@gmail.com>
2023-11-08 18:07:06 +0800

    Initial Commit

--- src/main/java/net/lavacreeper/vote/dao/ChoiceDao.java
+++ src/main/java/net/lavacreeper/vote/dao/ChoiceDao.java
@@ -6,6 +6,7 @@ import net.lavacreeper.vote.domain.Polls;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Update;
 import org.apache.ibatis.mapping.StatementType;
 
 import java.util.List;
@@ -16,5 +17,8 @@ public interface ChoiceDao {
     @Insert("insert into choices (poll_id, description) VALUES (#{polls_id}, #{description})")
     @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id", statementType = StatementType.PREPARED)
     public void save(Choices choices);
+
+    @Update("update choices set description = #{description} where id = #{id}")
+    public boolean update(Integer id, String description);
     public List<Choices> getByPollsId();
 }
