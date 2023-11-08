79e226ad2e9e4b6f03e988e0fc83a78db79c1700
lavacreeper <lavacreeper2333@gmail.com>
2023-11-08 18:05:06 +0800

    Initial Commit

--- src/main/java/net/lavacreeper/vote/dao/PollsDao.java
+++ src/main/java/net/lavacreeper/vote/dao/PollsDao.java
@@ -2,10 +2,7 @@ package net.lavacreeper.vote.dao;
 
 
 import net.lavacreeper.vote.domain.Polls;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 import org.apache.ibatis.mapping.StatementType;
 
 import java.util.List;
@@ -15,10 +12,10 @@ public interface PollsDao {
     @Insert("insert into polls (question, creator_id, end_date) VALUES (#{question}, #{creator_id},#{end_date})")
     @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id", statementType = StatementType.PREPARED)
     public void save(Polls polls);
-
     @Select("select * from polls where creator_id = #{userId}")
     public List<Polls> getPollsByUserId(Integer userId);
-
     @Select("select * from polls where id = #{id}")
     public Polls getPollsById(Integer id);
+    @Update("update polls set question = #{question} where id = #{id}")
+    public boolean update(Integer id,String question);
 }
