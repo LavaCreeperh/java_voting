b62470fe1b95cdda46f8b6252d9e988682ef194c
lavacreeper <lavacreeper2333@gmail.com>
2023-11-10 08:50:16 +0800

    11.9晚间推送

--- src/main/java/net/lavacreeper/vote/controller/restcontrollers/api/personal/GetPollsById.java
+++ src/main/java/net/lavacreeper/vote/controller/restcontrollers/api/personal/GetPollsById.java
@@ -1,33 +1,62 @@
 package net.lavacreeper.vote.controller.restcontrollers.api.personal;
 
-
-import net.lavacreeper.vote.domain.IdJson;
 import net.lavacreeper.vote.domain.Polls;
 import net.lavacreeper.vote.service.PollsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
-//这个是查看自己创建的投票的api
-//确保用户登陆
 @RestController
 public class GetPollsById {
     @Autowired
     PollsService pollsService;
 
-    @PostMapping("/api/getPollsById")
-    public List<Polls> getPollsById(@RequestBody IdJson id, HttpSession session) {
+    @GetMapping("/api/getPollsById")
+    public List<Polls> getPollsById(HttpSession session) {
         Integer userId = (Integer) session.getAttribute("USER_ID");
         if (userId == null) {
-            return null;
+            // 可以选择返回空列表，或者适当的错误响应
+            return null; // 或者使用 ResponseEntity 来返回一个错误响应
         }
-        if (!userId.equals(id.getId())) {
-            return null;
-        }
-        return pollsService.getPollsByUserId(id.getId());
+
+        return pollsService.getPollsByUserId(userId);
     }
 }
+
+
+//package net.lavacreeper.vote.controller.restcontrollers.api.personal;
+//
+//
+//import net.lavacreeper.vote.domain.IdJson;
+//import net.lavacreeper.vote.domain.Polls;
+//import net.lavacreeper.vote.service.PollsService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import javax.servlet.http.HttpSession;
+//import java.util.List;
+//
+////这个是查看自己创建的投票的api
+////确保用户登陆
+//@RestController
+//public class GetPollsById {
+//    @Autowired
+//    PollsService pollsService;
+//
+//    @PostMapping("/api/getPollsById")
+//    public List<Polls> getPollsById(@RequestBody IdJson id, HttpSession session) {
+//        Integer userId = (Integer) session.getAttribute("USER_ID");
+//        if (userId == null) {
+//            return null;
+//        }
+//        if (!userId.equals(id.getId())) {
+//            return null;
+//        }
+//        return pollsService.getPollsByUserId(id.getId());
+//    }
+//}
