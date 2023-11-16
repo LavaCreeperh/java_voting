292675bd36cc4dfa5249b827ef6d34510ad03c9a
lavacreeper <lavacreeper2333@gmail.com>
2023-11-16 15:20:15 +0800

    更新功能完成，注册功能完成

--- src/main/java/net/lavacreeper/vote/controller/restcontrollers/api/everyone/CheckIfUsernameExists.java
+++ src/main/java/net/lavacreeper/vote/controller/restcontrollers/api/everyone/CheckIfUsernameExists.java
@@ -2,7 +2,6 @@ package net.lavacreeper.vote.controller.restcontrollers.api.everyone;
 
 
 import net.lavacreeper.vote.domain.IfExistsApiMessage;
-import net.lavacreeper.vote.domain.UsernameJson;
 import net.lavacreeper.vote.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,12 +12,12 @@ public class CheckIfUsernameExists {
     UserService userService;
 
     //DONE 改为get方法
-    @GetMapping("/api/exist/{id}")
-    public IfExistsApiMessage checkIfUsernameExists(@PathVariable String id) {
-        if (!id.matches("^[A-Za-z0-9]+$")) {
+    @GetMapping("/api/exist/{username}")
+    public IfExistsApiMessage checkIfUsernameExists(@PathVariable String username) {
+        if (!username.matches("^[A-Za-z0-9]+$")) {
             return new IfExistsApiMessage("输入不合法", false);
         }
-        return userService.checkIfUsernameExists(id);
+        return userService.checkIfUsernameExists(username);
     }
 
 }
